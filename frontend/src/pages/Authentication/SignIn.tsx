3cd5f38b2702abd2d32f8e7fff28d78c27adb079
Esraa R. Mashaal <31254134+EsraaRMashaal@users.noreply.github.com>
2024-12-23 20:04:24 +0200

    fix: update password input placeholder for clarity

--- frontend/src/pages/Authentication/SignIn.tsx
+++ frontend/src/pages/Authentication/SignIn.tsx
@@ -119,7 +119,7 @@ const SignIn = () => {
                       autoComplete="current-password"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
-                      placeholder="6+ Characters, 1 Capital letter"
+                      placeholder="Enter your password"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                     />
 
